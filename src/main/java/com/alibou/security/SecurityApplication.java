e3c91e7d06eb327b2c02a03fb06336f7f02f86dc
mukhammadqodir <realking1007@gmail.com>
2024-02-05 17:37:14 +0500

    commited

--- src/main/java/com/alibou/security/SecurityApplication.java
+++ src/main/java/com/alibou/security/SecurityApplication.java
@@ -1,48 +1,46 @@
 package com.alibou.security;
 
 import com.alibou.security.auth.AuthenticationService;
-import com.alibou.security.auth.RegisterRequest;
-import com.alibou.security.user.Role;
+import com.alibou.security.user.User;
+import com.alibou.security.user.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.alibou.security.user.Role.ADMIN;
-import static com.alibou.security.user.Role.MANAGER;
 
 @SpringBootApplication
+@AllArgsConstructor
+
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SecurityApplication {
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(
-//			AuthenticationService service
-//	) {
-//		return args -> {
-//			var admin = RegisterRequest.builder()
-//					.firstname("Admin")
-//					.lastname("Admin")
-//					.email("admin@mail.com")
-//					.password("password")
-//					.role(ADMIN)
-//					.build();
-//			System.out.println("Admin token: " + service.register(admin).getAccessToken());
-//
-//			var manager = RegisterRequest.builder()
-//					.firstname("Admin")
-//					.lastname("Admin")
-//					.email("manager@mail.com")
-//					.password("password")
-//					.role(MANAGER)
-//					.build();
-//			System.out.println("Manager token: " + service.register(manager).getAccessToken());
-//
-//		};
-//	}
+
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+    ) {
+        return args -> {
+
+
+            User otabek = User.builder().role(ADMIN)
+                    .email("xakimovoatbek@gmail.com")
+                    .firstname("ernf")
+                    .lastname("wrkjfn")
+                    .password(passwordEncoder.encode("otabek"))
+                    .enabled(true).build();
+            userRepository.save(otabek);
+
+        };
+    }
 }
